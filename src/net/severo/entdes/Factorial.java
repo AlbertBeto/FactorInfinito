7f128a4f24d4e581bd7d5d153406a4684706a015
Beto <beto@beto.cat>
2023-01-29 19:35:01 +0100

    Refactorizacion texto a constantes.

--- src/net/severo/entdes/Factorial.java
+++ src/net/severo/entdes/Factorial.java
@@ -7,14 +7,15 @@ package net.severo.entdes;
 public class Factorial {
 
 	public static void main(String[] args) {
-
+String texto="Valueo of n: ";
+String texto2="Valueo of n!: ";
 		int n = (int) (Math.random() * 10);
 		int result = 1;
-		System.out.println("Value of n: " + n);
+		System.out.println(texto + n);
 		while (n > 1) {
 			result = result * (n - 1);
 		}
-		System.out.println("Value of n!: " + result);
+		System.out.println(texto2 + result);
 
 	}
 
